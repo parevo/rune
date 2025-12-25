30e34c6b1304f1ff4cd81df044636794d0ccf78b
ahmetbilgay <canahmetbilgay@gmail.com>
2025-12-25 03:11:13 +0300

    fix: update self-update slug from parevo-lab to parevo

--- database/updater.go
+++ database/updater.go
@@ -67,7 +67,7 @@ func (u *Updater) CheckForUpdate(currentVersion string) (*UpdateInfo, error) {
 }
 
 func (u *Updater) ApplyUpdate(latestVersion string) error {
-	slug := selfupdate.ParseSlug("parevo-lab/mergen")
+	slug := selfupdate.ParseSlug("parevo/mergen")
 
 	updater, err := selfupdate.NewUpdater(selfupdate.Config{
 		Filters:       []string{"^mergen_"},
